cfa5f0c7acc6cd0092d4958bde5ac7eb880a0177
Marcus Mayorga <mayorga.ml@gmail.com>
2020-02-25 16:55:20 -0800

    Adding functions

--- shopping.js
+++ shopping.js
@@ -6,12 +6,13 @@ const user = {
     cart: [],
     purchases: []
   }
- purchaseItem(user);
+ cartItem(user);
  buyItem(user);
+ // refunds(user);
  console.log('my cart', user.cart);
  console.log('my buys', user.purchases);
 }
-  function purchaseItem (user) {
+  function cartItem (user) {
      let item = [
           {name: 'Darts',
           price: 20},
@@ -35,10 +36,19 @@ const user = {
     for (item in cartItems){
     let purchaseItems = user.purchases;
         purchaseItems.push(cartItems[item]);
-        cartItems.length = 0;
+        if (purchaseItems == cartItems.length) {
+          cartItems.length = 0;
+        }
     }
   }
  cart();
+ 
+  // function refunds(user) {
+  //   let user = user;
+  //   let refund = {refunds: []};
+  //   user.push(refund);
+    
+  // }
   //Implement a cart feature:
   // 1. Add items to cart.
   // 2. Add 3% tax to item in cart
